5f3786997d6d5aedb196bd2f53e7ab32d33b4a5b
Aura <kyradiscord@gmail.com>
2024-05-16 20:56:46 +0200

    fix: split CJS and ESM builds correctly (#295)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'tsup';
+import { Options, defineConfig } from 'tsup';
 
-export default defineConfig({
+const baseOptions: Options = {
 	clean: true,
 	dts: true,
 	entry: ['src/index.ts'],
-	format: ['esm', 'cjs'],
 	minify: false,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
 	target: 'es2020',
 	tsconfig: 'src/tsconfig.json',
-	keepNames: true
-});
+	keepNames: true,
+	treeshake: true
+};
+
+export default [
+	defineConfig({ ...baseOptions, outDir: 'dist/cjs', format: 'cjs' }),
+	defineConfig({ ...baseOptions, outDir: 'dist/esm', format: 'esm', outExtension: () => ({ js: '.mjs' }) })
+];
